bd4986581af3228881f05aa2a2aab41cd0f81689
aldus <info@drp-x-work.de>
2014-10-10 13:54:35 +0200

    Minor changes and add missing file.

--- register_class_secure.php
+++ register_class_secure.php
@@ -1,19 +1,18 @@
 <?php
 
 /**
- *  @module         code2
+ *  @module         pagecloner
  *  @version        see info.php of this module
- *  @authors        Ryan Djurovich, Chio Maisriml, Thomas Hornik, Dietrich Roland Pehlke
- *  @copyright      2004-2014 Ryan Djurovich, Chio Maisriml, Thomas Hornik, Dietrich Roland Pehlke
+ *  @authors        John Maats - Dietrich Roland Pehlke - Stephan Kuehn - vBoedefeld, cms-lab
+ *  @copyright      2006-2010 John Maats - Dietrich Roland Pehlke - Stephan Kuehn - vBoedefeld
+ *  @copyright      2010-2014 cms-lab 
  *  @license        GNU General Public License
  *  @license terms  see info.php of this module
- *  @platform       see info.php of this module
  *
  */
 
 global $lepton_filemanager;
 if (!is_object($lepton_filemanager)) require_once( "../../framework/class.lepton.filemanager.php" );
-
 
 $files_to_register = array(
 	'/modules/pagecloner/tool_clone.php',
