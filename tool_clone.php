5870453565f63d913187930f65994c45c0eb71d9
aldus <info@drp-x-work.de>
2014-10-12 11:09:56 +0200

    Codechanges and remove old WB 2.7 and prior code.

--- tool_clone.php
+++ tool_clone.php
@@ -39,29 +39,17 @@ $pagetodo = isset($_GET['pagetoclone']) ? (int) $_GET['pagetoclone'] : 0;
 // check if specified page exists in the database
 $query = "SELECT * FROM ".TABLE_PREFIX."pages WHERE page_id = '$pagetodo'";
 $get_pagetodo = $database->query($query);   
-$is_pagetodo = $get_pagetodo->fetchRow();
+$is_pagetodo = $get_pagetodo->fetchRow( MYSQL_ASSOC );
 
-// check website baker platform (with WB 2.7, Admin-Tools were moved out of settings dialogue)
-$admintool_link = ADMIN_URL .'/settings/index.php?advanced=yes#administration_tools"';
-$pageclone_link = ADMIN_URL .'/settings/tool.php?tool=pagecloner';
-if(file_exists(ADMIN_PATH .'/admintools/tool.php')) {
-	$admintool_link = ADMIN_URL .'/admintools/index.php';
-	$pageclone_link = ADMIN_URL .'/admintools/tool.php?tool=pagecloner';
-}
+$admintool_link = ADMIN_URL .'/admintools/index.php';
+$pageclone_link = ADMIN_URL .'/admintools/tool.php?tool=pagecloner';
 
 // redirect to pageclone main page if no valid page was specified
 if ($pagetodo < 1 || !$is_pagetodo) {
 	die(header('Location: '.$pageclone_link));
 } 
 
-// create admin object depending on platform (admin tools were moved out of settings with WB 2.7)
-if(file_exists(ADMIN_PATH .'/admintools/tool.php')) {
-	// since Website Baker 2.7
-	$admin = new admin('admintools', 'admintools');
-} else {
-	// Website Baker prior to 2.7
-	$admin = new admin('Settings', 'settings_advanced');
-}
+$admin = new admin('admintools', 'admintools');
 
 /**
  *	Load Language file
@@ -82,115 +70,115 @@ if ($pagetodo > 0 && $is_pagetodo) {
 	<?php
 
 
-// Setup template object
-$template = new Template(LEPTON_PATH.'/modules/pagecloner/templates');
-$template->set_file('page', 'template.lte');
-$template->set_block('page', 'main_block', 'main');
+	// Setup template object
+	$template = new Template(LEPTON_PATH.'/modules/pagecloner/templates');
+	$template->set_file('page', 'template.lte');
+	$template->set_block('page', 'main_block', 'main');
 
-// Parent page list
+	// Parent page list
 
-function parent_list($parent) {
-	global $admin, $database, $template;
+	function parent_list($parent) {
+		global $admin, $database, $template;
 	
-	$admin_group_id = $admin->get_group_id();
-	$admin_user_id = $admin->get_user_id();
+		$admin_group_id = $admin->get_group_id();
+		$admin_user_id = $admin->get_user_id();
 	
-	$all_pages = array();
+		$all_pages = array();
 	
-	$database->execute_query(
-		"SELECT * FROM `".TABLE_PREFIX."pages` WHERE `parent`='".$parent."' AND `visibility` != 'deleted' ORDER BY position ASC",
-		true,
-		$all_pages
-	);
+		$database->execute_query(
+			"SELECT * FROM `".TABLE_PREFIX."pages` WHERE `parent`='".$parent."' AND `visibility` != 'deleted' ORDER BY position ASC",
+			true,
+			$all_pages
+		);
 	
-	foreach($all_pages as $page) {
-		// Stop users from adding pages with a level of more than the set page level limit
-		if( $page['level']+1 <= PAGE_LEVEL_LIMIT) {
-			// Get user perms
-			$admin_groups = explode(',', str_replace('_', '', $page['admin_groups']));
-			$admin_users = explode(',', str_replace('_', '', $page['admin_users']));
-			if(is_numeric(array_search($admin_group_id, $admin_groups)) OR is_numeric(array_search($admin_user_id, $admin_users))) {
-				$can_modify = true;
-			} else {
-				$can_modify = false;
-			}
-			// Title -'s prefix
-			for($i = 1, $title_prefix = ''; $i <= $page['level']; $i++) { $title_prefix .= ' - '; }
+		foreach($all_pages as $page) {
+			// Stop users from adding pages with a level of more than the set page level limit
+			if( $page['level']+1 <= PAGE_LEVEL_LIMIT) {
+				// Get user perms
+				$admin_groups = explode(',', str_replace('_', '', $page['admin_groups']));
+				$admin_users = explode(',', str_replace('_', '', $page['admin_users']));
+				if(is_numeric(array_search($admin_group_id, $admin_groups)) OR is_numeric(array_search($admin_user_id, $admin_users))) {
+					$can_modify = true;
+				} else {
+					$can_modify = false;
+				}
+				// Title -'s prefix
+				for($i = 1, $title_prefix = ''; $i <= $page['level']; $i++) { $title_prefix .= ' - '; }
 				
-			$template->set_var(array(
-				'ID' => $page['page_id'],
-				'TITLE' => ($title_prefix.$page['page_title'])
-				)
-			);
-			if( $can_modify == true) {
-				$template->set_var('DISABLED', '');
-			} else {
-				$template->set_var('DISABLED', ' disabled');
+				$template->set_var(array(
+					'ID' => $page['page_id'],
+					'TITLE' => ($title_prefix.$page['page_title'])
+					)
+				);
+				if( $can_modify == true) {
+					$template->set_var('DISABLED', '');
+				} else {
+					$template->set_var('DISABLED', ' disabled');
+				}
+				$template->parse('page_list2', 'page_list_block2', true);
 			}
-			$template->parse('page_list2', 'page_list_block2', true);
+			parent_list($page['page_id']);
 		}
-		parent_list($page['page_id']);
 	}
-}
 
-$template->set_block('main_block', 'page_list_block2', 'page_list2');
-if($admin->get_permission('pages_add_l0') == true) {
+	$template->set_block('main_block', 'page_list_block2', 'page_list2');
+	if($admin->get_permission('pages_add_l0') == true) {
+		$template->set_var(array(
+				'ID' => '0',
+				'TITLE' => $TEXT['NONE'],
+				'SELECTED' => ' selected',
+				'DISABLED' => ''
+			)
+		);
+		$template->parse('page_list2', 'page_list_block2', true);
+	}
+
+	parent_list(0);
+
+	// Insert language headings
 	$template->set_var(array(
-			'ID' => '0',
-			'TITLE' => $TEXT['NONE'],
-			'SELECTED' => ' selected',
-			'DISABLED' => ''
+		'HEADING_ADD_PAGE' => $MOD_PAGECLONER['CLONE_PAGETO'],
+		'HEADING_MODIFY_INTRO_PAGE' => $HEADING['MODIFY_INTRO_PAGE']
 		)
 	);
-	$template->parse('page_list2', 'page_list_block2', true);
-}
 
-parent_list(0);
+	// Insert language text and messages
+	$template->set_var(array(
+		'TEXT_TITLE' => $TEXT['TITLE'],
+		'TEXT_DEFAULT' => $is_pagetodo['menu_title'],
+		'TEXT_TYPE' => $TEXT['TYPE'],
+		'TEXT_PARENT' => $TEXT['PARENT'],
+		'TEXT_INCLUDE_SUBS' => $MOD_PAGECLONER['INCLUDE_SUBS'],
+		'TEXT_VISIBILITY' => $TEXT['VISIBILITY'],
+		'TEXT_PUBLIC' => $TEXT['PUBLIC'],
+		'TEXT_PRIVATE' => $TEXT['PRIVATE'],
+		'TEXT_REGISTERED' => $TEXT['REGISTERED'],
+		'TEXT_HIDDEN' => $TEXT['HIDDEN'],
+		'TEXT_NONE' => $TEXT['NONE'],
+		'TEXT_NONE_FOUND' => $TEXT['NONE_FOUND'],
+		'TEXT_PAGETODO' => $pagetodo,
+		'TEXT_ADD' => $MOD_PAGECLONER['ADD'],
+		'TEXT_RESET' => $TEXT['RESET'],
+		'TEXT_ADMINISTRATORS' => $TEXT['ADMINISTRATORS'],								
+		'TEXT_PRIVATE_VIEWERS' => $TEXT['PRIVATE_VIEWERS'],
+		'TEXT_REGISTERED_VIEWERS' => $TEXT['REGISTERED_VIEWERS'],
+		'INTRO_LINK' => $MESSAGE['PAGES']['INTRO_LINK'],
+		)
+	);
 
-// Insert language headings
-$template->set_var(array(
-	'HEADING_ADD_PAGE' => $MOD_PAGECLONER['CLONE_PAGETO'],
-	'HEADING_MODIFY_INTRO_PAGE' => $HEADING['MODIFY_INTRO_PAGE']
-	)
-);
+	// Insert permissions values
+	if($admin->get_permission('pages_add') != true) {
+		$template->set_var('DISPLAY_ADD', 'hide');
+	} elseif($admin->get_permission('pages_add_l0') != true AND $editable_pages == 0) {
+		$template->set_var('DISPLAY_ADD', 'hide');
+	}
+	if($admin->get_permission('pages_intro') != true OR INTRO_PAGE != 'enabled') {
+		$template->set_var('DISPLAY_INTRO', 'hide');
+	}
 
-// Insert language text and messages
-$template->set_var(array(
-	'TEXT_TITLE' => $TEXT['TITLE'],
-	'TEXT_DEFAULT' => $is_pagetodo['menu_title'],
-	'TEXT_TYPE' => $TEXT['TYPE'],
-	'TEXT_PARENT' => $TEXT['PARENT'],
-	'TEXT_INCLUDE_SUBS' => $MOD_PAGECLONER['INCLUDE_SUBS'],
-	'TEXT_VISIBILITY' => $TEXT['VISIBILITY'],
-	'TEXT_PUBLIC' => $TEXT['PUBLIC'],
-	'TEXT_PRIVATE' => $TEXT['PRIVATE'],
-	'TEXT_REGISTERED' => $TEXT['REGISTERED'],
-	'TEXT_HIDDEN' => $TEXT['HIDDEN'],
-	'TEXT_NONE' => $TEXT['NONE'],
-	'TEXT_NONE_FOUND' => $TEXT['NONE_FOUND'],
-	'TEXT_PAGETODO' => $pagetodo,
-	'TEXT_ADD' => $MOD_PAGECLONER['ADD'],
-	'TEXT_RESET' => $TEXT['RESET'],
-	'TEXT_ADMINISTRATORS' => $TEXT['ADMINISTRATORS'],								
-	'TEXT_PRIVATE_VIEWERS' => $TEXT['PRIVATE_VIEWERS'],
-	'TEXT_REGISTERED_VIEWERS' => $TEXT['REGISTERED_VIEWERS'],
-	'INTRO_LINK' => $MESSAGE['PAGES']['INTRO_LINK'],
-	)
-);
-
-// Insert permissions values
-if($admin->get_permission('pages_add') != true) {
-	$template->set_var('DISPLAY_ADD', 'hide');
-} elseif($admin->get_permission('pages_add_l0') != true AND $editable_pages == 0) {
-	$template->set_var('DISPLAY_ADD', 'hide');
-}
-if($admin->get_permission('pages_intro') != true OR INTRO_PAGE != 'enabled') {
-	$template->set_var('DISPLAY_INTRO', 'hide');
-}
-
-// Parse template object
-$template->parse('main', 'main_block', false);
-$template->pparse('output', 'page');
+	// Parse template object
+	$template->parse('main', 'main_block', false);
+	$template->pparse('output', 'page');
 }	
 $admin->print_footer();
 
