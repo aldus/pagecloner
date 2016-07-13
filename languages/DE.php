1716715985b6e1861db123ceae787f64e6a2c8c9
labby <rp@cms-lab.com>
2016-07-13 10:16:22 +0200

    add language file

---
+++ languages/DE.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ *  @module         pagecloner
+ *  @version        see info.php of this module
+ *  @authors        John Maats - Dietrich Roland Pehlke - Stephan Kuehn - vBoedefeld, cms-lab
+ *  @copyright      2006-2010 John Maats - Dietrich Roland Pehlke - Stephan Kuehn - vBoedefeld
+ *  @copyright      2010-2016 cms-lab 
+ *  @license        GNU General Public License
+ *  @license terms  see info.php of this module
+ *
+ */
+
+// German language file for the admin module 'pagecloner'
+
+$MOD_PAGECLONER['INTRO_TEXT']		= 'Mit diesem Addon kannst du Seiten kopieren.<br />Nachfolgend eine Liste aller vorhandenen Seiten:';
+$MOD_PAGECLONER['CHOOSE_PAGE']		= 'Wähle eine zu kopierende Seite';
+$MOD_PAGECLONER['CLONE_PAGE']		= 'Klick um die derzeitige Seite zu kopieren!';
+$MOD_PAGECLONER['CLONE_PAGETO']		= 'Kopiere aktuelle Seite zu: ';
+$MOD_PAGECLONER['CLONE_FROM']		= 'Kopieren : ';
+$MOD_PAGECLONER['CLONE_TO']			= 'Zu neuer Seite: ';
+$MOD_PAGECLONER['INCLUDE_SUBS']		= 'Unterseiten einbeziehen? ';
+$MOD_PAGECLONER['ADD']				= 'Kopieren';
+$MOD_PAGECLONER['CONTINUE']			= 'Fortfahren';
+$MOD_PAGECLONER['ABORT']			= 'Abbrechen';
+$MOD_PAGECLONER['CREATED']			= 'Erfolgreich kopiert';
+$MOD_PAGECLONER['INCLUDE_SUBS']     = 'einschließlich Unterseiten';
+
+?>
